4c573cf635cf80e795565a21d99b146896522467
Julistian <urist9112@rambler.ru>
2017-04-05 13:34:56 +0300

    Была немного изменена вёрстка для удобства биндинга. Была создана ViewModel. Был осуществлён биндинг.

---
+++ Lab2/Lab2/CharacterCreatorViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class CharacterCreatorViewModel
+    {
+        public byte Force { get; set; }
+        public byte Agility { get; set; }
+        public byte Intelligence { get; set; }
+        public byte Luck { get; set; }
+        public byte FreePoints { get; set; }
+        public string Nickname { get; set; }
+        public List<string> CharacterClass { get; set; }
+        public string SelectedCharacterClass { get; set; }
+        public CharacterCreatorViewModel()
+        {
+            CharacterClass = new List<string>()
+            {
+                "Файтер",
+                "Клирик",
+                "Вор",
+                "Маг"
+            };
+        }
+    }
+}
