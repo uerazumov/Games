9ec0eae2fcb9bae6dbc966bdc586f4c526890d9c
Julistian <urist9112@rambler.ru>
2017-09-24 21:04:37 +0300

    Начата разработка курсового проекта
    Созданы шаблоны главной страницы и страницы игры

---
+++ OneQuestionFourAnswers/OneQuestionFourAnswers/MainMenuPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OneQuestionFourAnswers
+{
+    /// <summary>
+    /// Логика взаимодействия для MainMenuPage.xaml
+    /// </summary>
+    public partial class MainMenuPage : Page
+    {
+        public MainMenuPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_Start_Game(object sender, RoutedEventArgs e)
+        {
+            NavigationService ns = NavigationService.GetNavigationService(this);
+            ns.Navigate(new Uri("GamePage.xaml", UriKind.Relative));
+        }
+    }
+}
