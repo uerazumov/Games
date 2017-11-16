1779147e3d3312709374ab199e3aa5a4eefb5cf3
Julistian <urist9112@rambler.ru>
2017-11-17 00:49:39 +0300

    Внесены изменения в контрол для жизней

--- OneQuestionFourAnswers/OneQuestionFourAnswers/LifeIcon.xaml.cs
+++ OneQuestionFourAnswers/OneQuestionFourAnswers/LifeIcon.xaml.cs
@@ -14,7 +14,7 @@ namespace OneQuestionFourAnswers
             DataContext = this;
         }
 
-        public ImageBrush DisableImage = new ImageBrush(new BitmapImage(new Uri(@".\VisualResources\Images\LifeUsedIcon.png", UriKind.Relative)));
-        public ImageBrush EnableImage = new ImageBrush(new BitmapImage(new Uri(@".\VisualResources\Images\LifeIcon.png", UriKind.Relative)));
+        private ImageBrush DisableImage = new ImageBrush(new BitmapImage(new Uri(@".\VisualResources\Images\LifeUsedIcon.png", UriKind.Relative)));
+        private ImageBrush EnableImage = new ImageBrush(new BitmapImage(new Uri(@".\VisualResources\Images\LifeIcon.png", UriKind.Relative)));
     }
 }
