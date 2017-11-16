96434b733b80bd493178f79c807f1c4dea21fde4
Julistian <urist9112@rambler.ru>
2017-11-17 00:34:53 +0300

    Добавлен контрол для жизней

---
+++ OneQuestionFourAnswers/OneQuestionFourAnswers/LifeIcon.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace OneQuestionFourAnswers
+{
+    public partial class LifeIcon : UserControl
+    {
+        public LifeIcon()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public ImageBrush DisableImage = new ImageBrush(new BitmapImage(new Uri(@".\VisualResources\Images\LifeUsedIcon.png", UriKind.Relative)));
+        public ImageBrush EnableImage = new ImageBrush(new BitmapImage(new Uri(@".\VisualResources\Images\LifeIcon.png", UriKind.Relative)));
+    }
+}
