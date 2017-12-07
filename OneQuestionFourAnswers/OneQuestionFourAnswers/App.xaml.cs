d199da6ad60e77209f4602fe99484a27384ccad8
Юрий <urist9112@rambler.ru>
2017-12-07 22:49:28 +0300

    Добавлен глобальный обработчик ошибок

--- OneQuestionFourAnswers/OneQuestionFourAnswers/App.xaml.cs
+++ OneQuestionFourAnswers/OneQuestionFourAnswers/App.xaml.cs
@@ -1,6 +1,13 @@
-﻿namespace OneQuestionFourAnswers
+﻿using System.Windows;
+
+namespace OneQuestionFourAnswers
 {
-    public partial class App
+    public partial class App : Application
     {
+        private void ApplicationDispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("Notified of a thread exception... application is terminating." + e.Exception.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+        }
     }
 }
