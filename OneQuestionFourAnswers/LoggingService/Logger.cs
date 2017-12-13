564dda7c3348bb0462f1592c95a0b2838ff46216
Юрий <urist9112@rambler.ru>
2017-12-13 16:04:56 +0300

    Добавлен пакет для Логгера, исправлены небольшие баги

--- OneQuestionFourAnswers/LoggingService/Logger.cs
+++ OneQuestionFourAnswers/LoggingService/Logger.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
 
 namespace LoggingService
 {
     public class Logger
     {
-
+        private static Logger logger = LogManager.GetCurrentClassLogger();
     }
 }
