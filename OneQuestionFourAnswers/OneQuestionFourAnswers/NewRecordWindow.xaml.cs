9bbbe45dbc07119df1fdfaaf4d0e586c9b61c72b
Юрий <urist9112@rambler.ru>
2017-12-19 11:42:03 +0300

    Была добавлена многопоточность в окне Новый рекорд

--- OneQuestionFourAnswers/OneQuestionFourAnswers/NewRecordWindow.xaml.cs
+++ OneQuestionFourAnswers/OneQuestionFourAnswers/NewRecordWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -10,6 +11,7 @@ namespace OneQuestionFourAnswers
     {
         private TextBox _textBox;
         private readonly MainWindowViewModel _vm;
+        private byte CountOfAwaitings = 0;
 
         public NewRecordWindow()
         {
@@ -26,13 +28,50 @@ namespace OneQuestionFourAnswers
         }
         private void SaveButtonClick(object sender, RoutedEventArgs e)
         {
-            if (!Validation.GetHasError(_textBox)&&((_textBox).Text != ""))
+            if (!Validation.GetHasError(_textBox) && ((_textBox).Text != ""))
             {
-                DialogResult = true;
-                Close();
+                new Thread(() =>
+                {
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        MakeButtonDisabled(CloseButton);
+                        MakeButtonDisabled(SaveButton);
+                        WaitMessage.Visibility = Visibility.Visible;
+                        CountOfAwaitings++;
+                    });
+                    if (_vm.CreateNewRecord())
+                    {
+                        Application.Current.Dispatcher.Invoke(() =>
+                        {
+                            BorderUsernameTextBox.IsEnabled = false;
+                            NoticeTextBlock.Visibility = Visibility.Collapsed;
+                            RecordError.Visibility = Visibility.Collapsed;
+                            RecotdSuccessfully.Visibility = Visibility.Visible;
+                        });
+                    }
+                    else
+                    {
+                        Application.Current.Dispatcher.Invoke(() =>
+                        {
+                            MakeButtonEnabled(SaveButton);
+                            NoticeTextBlock.Visibility = Visibility.Collapsed;
+                            RecordError.Visibility = Visibility.Visible;
+                        });
+                    };
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        if (CountOfAwaitings-1 == 0)
+                        {
+                            WaitMessage.Visibility = Visibility.Collapsed;
+                            MakeButtonEnabled(CloseButton);
+                        }
+                        CountOfAwaitings--;
+                    });
+                }).Start();
             }
             else
             {
+                RecordError.Visibility = Visibility.Collapsed;
                 NoticeTextBlock.Visibility = Visibility.Visible;
             }
         }
@@ -50,52 +89,114 @@ namespace OneQuestionFourAnswers
             NoticeTextBlock.Visibility = Visibility.Collapsed;
         }
 
-        private void CancelButtonClick(object sender, RoutedEventArgs e)
+        private void CloseButtonClick(object sender, RoutedEventArgs e)
         {
-            DialogResult = false;
             Close();
         }
 
         private void SaveStatisticButtonClick(object sender, RoutedEventArgs e)
         {
-            var result = _vm.CreateReport();
-            if (result == true)
+            new Thread(() =>
             {
-                SaveStatistic.IsEnabled = false;
-                SaveStatistic.Foreground = Brushes.Gray;
-                ReportError.Visibility = Visibility.Collapsed;
-                ReportSuccessfully.Visibility = Visibility.Visible;
-            }
-            else if (result == false)
-            {
-                ReportError.Visibility = Visibility.Visible;
-            }
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    MakeButtonDisabled(CloseButton);
+                    MakeButtonDisabled(SaveStatistic);
+                    WaitMessage.Visibility = Visibility.Visible;
+                    CountOfAwaitings++;
+                });
+                var result = _vm.CreateReport();
+                if (result == true)
+                {
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        ReportError.Visibility = Visibility.Collapsed;
+                        ReportSuccessfully.Visibility = Visibility.Visible;
+                    }); 
+                }
+                else if (result == false)
+                {
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        MakeButtonEnabled(SaveStatistic);
+                        ReportError.Visibility = Visibility.Visible;
+                    });
+                }
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    if (CountOfAwaitings - 1 == 0)
+                    {
+                        WaitMessage.Visibility = Visibility.Collapsed;
+                        MakeButtonEnabled(CloseButton);
+                    }
+                    CountOfAwaitings--;
+                });
+            }).Start();
+        }
+
+        private void MakeButtonDisabled(Button button)
+        {
+            button.IsEnabled = false;
+            button.Foreground = Brushes.Gray;
+        }
+
+        private void MakeButtonEnabled(Button button)
+        {
+            button.IsEnabled = true;
+            button.Foreground = Brushes.White;
         }
 
         private void SaveRecordIntoVKButtonClick(object sender, RoutedEventArgs e)
         {
-            bool? webResult = true;
-            if (!_vm.IsTokenExist())
+            new Thread(() =>
             {
-                WebBrowser web = new WebBrowser();
-                web.ShowDialog();
-                web.Owner = Owner;
-                webResult = web.DialogResult;
-            }
-            if (webResult != null)
-            {
-                if ((webResult ?? false) && _vm.CreateRec())
+                Application.Current.Dispatcher.Invoke(() =>
                 {
-                    SaveRecordIntoVK.IsEnabled = false;
-                    SaveRecordIntoVK.Foreground = Brushes.Gray;
-                    VkError.Visibility = Visibility.Collapsed;
-                    VkSuccessfully.Visibility = Visibility.Visible;
-                }
-                else
+                    MakeButtonDisabled(CloseButton);
+                    MakeButtonDisabled(SaveRecordIntoVK);
+                    WaitMessage.Visibility = Visibility.Visible;
+                    CountOfAwaitings++;
+                });
+                bool? webResult = true;
+                if (!_vm.IsTokenExist())
                 {
-                    VkError.Visibility = Visibility.Visible;
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        WebBrowser web = new WebBrowser();
+                        web.ShowDialog();
+                        web.Owner = Owner;
+                        webResult = web.DialogResult;
+                    });
                 }
-            }
+                if (webResult != null)
+                {
+                    if ((webResult ?? false) && _vm.CreateRec())
+                    {
+                        Application.Current.Dispatcher.Invoke(() =>
+                        {
+                            VkError.Visibility = Visibility.Collapsed;
+                            VkSuccessfully.Visibility = Visibility.Visible;
+                        });
+                    }
+                    else
+                    {
+                        Application.Current.Dispatcher.Invoke(() =>
+                        {
+                            MakeButtonEnabled(SaveRecordIntoVK);
+                            VkError.Visibility = Visibility.Visible;
+                        });
+                    }
+                }
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    if (CountOfAwaitings - 1 == 0)
+                    {
+                        WaitMessage.Visibility = Visibility.Collapsed;
+                        MakeButtonEnabled(CloseButton);
+                    }
+                    CountOfAwaitings--;
+                });
+            }).Start();
         }
 
         private void OnWindowLoaded(object sender, RoutedEventArgs e)
